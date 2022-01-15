b2a8e77b20702a75e95207ef9ccfe14e211cd873
Kristaps Ritins <kritinsdev@gmail.com>
2022-01-15 18:58:17 +0200

    setup base controllers

---
+++ app/libraries/core.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * App Core Class
+ * Creates URL & loads core controller
+ * URL FORMAT - /controller/method/params
+ */
+
+class Core
+{
+    protected $currentController = 'Pages';
+
+    protected $currentMethod = 'index';
+
+    protected $params = [];
+
+    public function __construct()
+    {
+        $url = $this->getUrl();
+        
+        //Look in Controllers for first value
+        if (isset($url[0]) && file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+            //If file exists set as controller
+            $this->currentController = ucwords($url[0]);
+
+            //Unset 0 index
+            unset($url[0]);
+        }
+
+        //Require the controller
+        require_once '../app/controllers/' . $this->currentController . '.php';
+
+        //Instantiate it
+        $this->currentController = new $this->currentController;
+    }
+
+    public function getUrl()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+    }
+}
